e87e86fd2f40aeaa6f48d2109847a32c45eb7221
Jeffrey Sun <jsun624@gmail.com>
2020-04-21 17:33:08 -0700

    Refreshing a chat room no longer gives an error

---
+++ client/build/precache-manifest.6c68610dad79fb77a0a17f2b054bb00a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284506ba89ac9f4e69e19ad1eccd5bc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9777fa69d510a2e33f95",
+    "url": "/static/css/main.e6001e4b.chunk.css"
+  },
+  {
+    "revision": "f8803577dbb0c03f6c6d",
+    "url": "/static/js/2.daac4337.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.daac4337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9777fa69d510a2e33f95",
+    "url": "/static/js/main.9dbafa41.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
